e8439e5c82f8f73c089c52daa23725dcc128c0fa
Andrés Garelli <agarelli91@gmail.com>
2023-06-17 15:18:01 -0300

    correccion de favicon

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// variables de formulario
+let correoElectronico, nombreCompleto, nombreJugador, contraseña;
+
+// variables de juego
+let diamanteSabio, diamantePoeta, DiamanteActor, personaje, narrativa;
